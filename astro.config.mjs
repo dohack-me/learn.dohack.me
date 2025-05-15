4a4be54caac9bff3123ff18a1e338b3b49a8d9f5
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2025-05-16 00:52:28 +0800

    reorganize files and added some intro content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,54 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import {defineConfig} from "astro/config"
+import starlight from "@astrojs/starlight"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: "My Docs",
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/withastro/starlight",
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: "Introduction",
+					slug: "introduction"
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Binary Exploitation",
+					items: [
+						{
+							label: "Introduction",
+							slug: "pwn/introduction"
+
+						},
+						{
+							label: "Tooling",
+							slug: "pwn/tooling"
+
+						},
+						{
+							label: "Tutorial",
+							autogenerate: {
+								directory: "pwn/tutorials"
+							}
+						},
+						{
+							label: "Guides",
+							autogenerate: {
+								directory: "pwn/guides"
+							}
+						},
+						{
+							label: "Explanations",
+							autogenerate: {
+								directory: "pwn/explanations"
+							}
+						},
+					],
 				},
 			],
 		}),
 	],
-});
+})
