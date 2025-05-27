77bdb277ee77220ba41a38c43d335e028540d20d
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2025-05-27 21:44:05 +0800

    remove sitemap adapter lol starlight already has support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import {defineConfig} from "astro/config"
 import starlight from "@astrojs/starlight"
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://phosphor.diffusehyperion.com',
 	integrations: [
-		sitemap(),
 		starlight({
 			title: "Phosphor",
 			social: {
